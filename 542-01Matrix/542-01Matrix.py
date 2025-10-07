d7b00596f3ebb1ba57ee9616429c159a6b1e5b2b
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-06 18:37:06 -0700

    File updated at 2025/10/6 18:37:05

--- 542-01Matrix/542-01Matrix.py
+++ 542-01Matrix/542-01Matrix.py
@@ -1,37 +1,38 @@
-# Last updated: 2025/10/6 18:36:13
+# Last updated: 2025/10/6 18:37:05
 class Solution(object):
-    def search(self, nums, target):
+    def updateMatrix(self, mat):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
         """
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            next_idx = left + (right - left) // 2
-            if nums[left] <= nums[next_idx]:
-                if target > nums[next_idx] or target < nums[left]:
-                    left = next_idx + 1
-                    continue
-                right = next_idx
-                next_idx = left + (right - left) // 2
-                if target > nums[next_idx]:
-                    left = next_idx + 1
-                elif target < nums[next_idx]:
-                    right = next_idx - 1
-                else:
-                    return next_idx
-            else:
-                if target > nums[right] or target < nums[next_idx]:
-                    right = next_idx - 1
-                    continue
-                left = next_idx
-                next_idx = left + (right - left) // 2
-                if target > nums[next_idx]:
-                    left = next_idx + 1
-                elif target < nums[next_idx]:
-                    right = next_idx - 1
-                else:
-                    return next_idx
-        return -1
+        # Like rotten oranges problem
+        # First, classify two sets of verts by 0/1
+        # Second, no need to actually build graph,
+        # instead, using directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        # Finally, calculate distance using BFS
+        set_0 = deque()
+        m = len(mat)
+        n = len(mat[0])
+        res = [[-1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    set_0.append((i, j, 0))
+                    res[i][j] = 0
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        while set_0:
+            u, v, dis = set_0.popleft()
+            for dire in directions:
+                nu = u + dire[0]
+                nv = v + dire[1]
+                if nu >= 0 and nu < m and nv >= 0 and nv < n:
+                    if mat[nu][nv] == 1:
+                        mat[nu][nv] = 0
+                        if res[nu][nv] == -1:
+                            res[nu][nv] = dis + 1
+                        else:
+                            res[nu][nv] = min(dis + 1, mat[nu][nv])
+                        set_0.append((nu, nv, dis + 1))
+        return res
+
+        
