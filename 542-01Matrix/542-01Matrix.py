76762a55c145e53e5a8ba1ababc88982023d808f
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-06 18:02:39 -0700

    File updated at 2025/10/6 18:02:39

--- 542-01Matrix/542-01Matrix.py
+++ 542-01Matrix/542-01Matrix.py
@@ -1,4 +1,4 @@
-# Last updated: 2025/10/6 17:42:37
+# Last updated: 2025/10/6 18:02:39
 class Solution(object):
     def canPartition(self, nums):
         """
@@ -20,15 +20,32 @@ class Solution(object):
             return False
         else:
             target = sum_set / 2
-            dp = [[False for _ in range(target + 1)] for _ in range(len(nums) + 1)]
-        for i in range(len(nums) + 1):
-            dp[i][0] = True
-        for i in range(1, len(nums) + 1):
-            for v in range(target + 1):
-                if v >= nums[i - 1]:
-                    dp[i][v] = dp[i - 1][v - nums[i - 1]] or dp[i - 1][v]
-                else:
-                    dp[i][v] = dp[i - 1][v]
-        return dp[-1][target]
+        # 2-dim array
+        # dp = [[False for _ in range(target + 1)] for _ in range(len(nums) + 1)]
+        # for i in range(len(nums) + 1):
+        #     dp[i][0] = True
+        # for i in range(1, len(nums) + 1):
+        #     for v in range(target + 1):
+        #         if v >= nums[i - 1]:
+        #             dp[i][v] = dp[i - 1][v - nums[i - 1]] or dp[i - 1][v]
+        #         else:
+        #             dp[i][v] = dp[i - 1][v]
+        # return dp[-1][target]
+
+        # for 1-dim array
+        # dp[i] represents if numbers can be added to i
+        # dp[i] = dp[i - nums[j]] for j in range(len(nums))
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for i in range(len(nums)):
+            # why reverse order?
+            # every element can only be used once
+            for j in range(target, nums[i] - 1, -1):
+                # why 'or dp[j]'?
+                # with i++, dp[j] will be overwritten by wrong values
+                dp[j] = dp[j - nums[i]] or dp[j]
+        return dp[-1]
+
+
 
         
