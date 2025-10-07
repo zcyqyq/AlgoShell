31f738e63d6ec4783a2a530f8c797e123dc7b2ba
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-07 16:27:01 -0700

    Create 295-FindMedianfromDataStream

---
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -0,0 +1,21 @@
+# Last updated: 2025/10/7 16:27:01
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n = len(height)
+        max_left = [0] * n
+        max_right = [0] * n
+        max_right[n - 1] = height[n - 1]
+        rain = 0
+        for i in range(n):
+            max_left[i] = max(height[i], max_left[i - 1])
+        for i in range(n - 2, -1, -1):
+            max_right[i] = max(height[i], max_right[i + 1])
+        for i in range(n):
+            h = min(max_left[i], max_right[i])
+            rain += (h - height[i])
+        return rain
+
