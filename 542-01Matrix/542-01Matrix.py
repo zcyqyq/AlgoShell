c758c6c3521be59b91b398b03be44c89a5e9f316
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-06 19:17:17 -0700

    File updated at 2025/10/6 19:17:17

--- 542-01Matrix/542-01Matrix.py
+++ 542-01Matrix/542-01Matrix.py
@@ -1,4 +1,4 @@
-# Last updated: 2025/10/6 18:56:00
+# Last updated: 2025/10/6 19:17:17
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -9,11 +9,30 @@ class Solution(object):
 
         # First, 1-dim dp with no reverse order
         # but need to record
-        dp = [[] for _ in range(target + 1)]
-        dp[0] = [[]]
-        for i in range(len(candidates)):
-            for j in range(candidates[i], target + 1):
-                for comb in dp[j - candidates[i]]:
-                    dp[j].append(comb + [candidates[i]])
-        return dp[target]
-        
+        # easy but consume large memory
+        # dp = [[] for _ in range(target + 1)]
+        # dp[0] = [[]]
+        # for i in range(len(candidates)):
+        #     for j in range(candidates[i], target + 1):
+        #         for comb in dp[j - candidates[i]]:
+        #             dp[j].append(comb + [candidates[i]])
+        # return dp[target]
+
+        # try dfs
+        graph = defaultdict(list)
+        for i in range(target + 1):
+            for j in range(len(candidates)):
+                if i + candidates[j] <= target:
+                    graph[i].append(i + candidates[j])
+        res = []
+        def dfs(node, path, start):
+            if node == target:
+                res.append(path)
+                return
+            for idx in range(start, len(candidates)):
+                num = candidates[idx]
+                if node + num <= target:
+                    dfs(node + num, path + [num], idx) 
+        dfs(0, [], 0)
+        return res
+            
