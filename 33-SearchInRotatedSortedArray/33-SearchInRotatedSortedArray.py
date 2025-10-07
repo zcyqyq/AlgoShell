34ab9deff2a15bbed4b2226a71643dc20c256a19
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-06 18:36:25 -0700

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.py
@@ -0,0 +1,37 @@
+# Last updated: 2025/10/6 18:36:25
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            next_idx = left + (right - left) // 2
+            if nums[left] <= nums[next_idx]:
+                if target > nums[next_idx] or target < nums[left]:
+                    left = next_idx + 1
+                    continue
+                right = next_idx
+                next_idx = left + (right - left) // 2
+                if target > nums[next_idx]:
+                    left = next_idx + 1
+                elif target < nums[next_idx]:
+                    right = next_idx - 1
+                else:
+                    return next_idx
+            else:
+                if target > nums[right] or target < nums[next_idx]:
+                    right = next_idx - 1
+                    continue
+                left = next_idx
+                next_idx = left + (right - left) // 2
+                if target > nums[next_idx]:
+                    left = next_idx + 1
+                elif target < nums[next_idx]:
+                    right = next_idx - 1
+                else:
+                    return next_idx
+        return -1
