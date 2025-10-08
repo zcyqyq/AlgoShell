7e6d168cfbb3ca19588e3b4367f9cfbf5b8f645d
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-07 17:13:52 -0700

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -0,0 +1,14 @@
+# Last updated: 2025/10/7 17:13:52
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = 0
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+        return max(dp)
+        
