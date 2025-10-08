96905d342f2affdcecf444a4a7f42eee6d9f7dac
Caiyi Zhang <zcyqyq@gmail.com>
2025-10-07 17:46:19 -0700

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -0,0 +1,26 @@
+# Last updated: 2025/10/7 17:46:18
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        left = -1
+        right = -1
+        res = []
+        intervals = sorted(intervals)
+        for interval in intervals:
+            if right < interval[0]:
+                # if not initial value
+                if left != -1:
+                    res.append([left, right])
+                left = interval[0]
+                right = interval[1]
+                continue
+            if interval[0] >= left and interval[0] <= right:
+                if interval[1] > right:
+                    right = interval[1]
+                    continue
+        res.append([left, right])
+        return res
+        
